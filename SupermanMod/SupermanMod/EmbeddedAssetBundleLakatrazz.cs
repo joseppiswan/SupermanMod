b7f9cbcb45c30fb590a9799d9a7663ef7f2fce76
Joe Swanson <73356433+joseppiswan@users.noreply.github.com>
2022-12-26 11:17:44 -0800

    base w.i.p. heat vision component + eab by lak

---
+++ SupermanMod/SupermanMod/EmbeddedAssetBundleLakatrazz.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+using System.Reflection;
+using System.IO;
+
+using UnityEngine;
+
+using MelonLoader;
+using System;
+using UnhollowerRuntimeLib;
+
+namespace SuperMan
+{
+    public static class EmebeddedAssetBundleLakatrazz
+    {
+
+        public static AssetBundle LoadFromAssembly(Assembly assembly, string name)
+        {
+            string[] manifestResources = assembly.GetManifestResourceNames();
+
+            if (manifestResources.Contains(name))
+            {
+                MelonLogger.Msg($"Loading embedded bundle data {name}...", ConsoleColor.DarkCyan);
+
+                byte[] bytes;
+                using (Stream str = assembly.GetManifestResourceStream(name))
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    str.CopyTo(memoryStream);
+                    bytes = memoryStream.ToArray();
+                }
+
+                MelonLogger.Msg($"Loading bundle from data {name}, please be patient...", ConsoleColor.DarkCyan);
+                var temp = AssetBundle.LoadFromMemory(bytes);
+                MelonLogger.Msg($"Done!", ConsoleColor.DarkCyan);
+                return temp;
+            }
+
+            return null;
+        }
+        internal static void LoadAssetBundle(Assembly executingAssembly,HeatVisionLaserComponent yes)
+        {
+            AssetBundle bundle = LoadFromAssembly(executingAssembly, "SuperMan.Resources.supermanmod.superman_assets");
+            if (bundle == null)
+            {
+                throw new NullReferenceException("ASSET BUNDLE NULL; YOU SUCK AT PROGRAMMING JOE");
+            }
+            LineRenderer render = UnityEngine.Object.Instantiate(bundle.LoadAsset("HEAT_LASER", Il2CppType.Of<GameObject>()).Cast<GameObject>()).GetComponent<LineRenderer>();
+            yes.heatLaser = render;
+            yes.laserObject = render.gameObject;
+            render.hideFlags = HideFlags.DontUnloadUnusedAsset;
+            MelonLogger.Msg(render.name);
+            bundle.Unload(false);
+        }
+    }
+}
---
+++ SupermanMod/SupermanMod/HeatVisionLaserComponent.cs
@@ -0,0 +1,67 @@
+﻿using MelonLoader;
+using System;
+using System.Reflection;
+using SuperMan;
+using UnityEngine;
+
+namespace SuperMan
+{
+    [RegisterTypeInIl2Cpp]
+    class HeatVisionLaserComponent : MonoBehaviour
+    {
+        public HeatVisionLaserComponent(IntPtr ptr) : base(ptr) { }
+
+        public GameObject laserObject;
+        public LineRenderer heatLaser;
+        public GameObject hitObject;
+        public Vector3 startPos;
+        public Vector3 endPos;
+
+        public bool enabled;
+        void Start()
+        {
+            SetupLaser();
+            heatLaser.positionCount = 0;
+        }
+        void Update()
+        {
+            RaycastHit h;
+            if (!Physics.Raycast(startPos, transform.forward, out h, 10000f, 8))
+                return;
+            startPos = transform.position - new Vector3(0, 0.1f, 0);
+            endPos = h.point;
+            MelonLogger.Msg("got past raycast");
+        }
+        void LateUpdate()
+        {
+            if (!laserObject.GetComponent<LineRenderer>()) 
+                return;
+            laserObject.GetComponent<LineRenderer>().SetPositions(new Vector3[] {
+                startPos,
+                endPos
+            });
+        }
+        private LineRenderer GetLaserRenderer()
+        {
+            return heatLaser;
+        }
+        public void SetupLaser()
+        {
+            Assembly executing = Assembly.GetExecutingAssembly();
+            EmebeddedAssetBundleLakatrazz.LoadAssetBundle(executing,this);
+        }
+        public void EnableLaser()
+        {
+            heatLaser.positionCount = 2;
+        }
+        public void EndLaser()
+        {
+            heatLaser.positionCount = 0;
+        }
+        public void DestroyLaser()
+        {
+            Destroy(heatLaser);
+            Destroy(this);
+        }
+    }
+}
